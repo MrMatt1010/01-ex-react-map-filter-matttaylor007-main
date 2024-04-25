0045bc6e573a94b57b53c2c27989fba07413ea76
Matt “Kalyp3r” Taylor <mrsymtec@gmail.com>
2024-04-26 11:00:49 +1200

    first commit

---
+++ src/utils/filterByCategory.js
@@ -0,0 +1,11 @@
+const filterByCategory = (products, filterBy) => {
+  if (filterBy === "all") {
+    return products;
+  }
+
+  return products.filter((product) => {
+    return product.metadata.category === filterBy;
+  });
+};
+
+export default filterByCategory;
